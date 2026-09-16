f1e4f699dacd65f25cda690c85284ad6d14d52d4
Boddupalli Venkata Harika <bvharika0906@gmail.com>
2026-09-16 12:42:31 +0530

    ✅ First Unique Character in a String | Easy | Java | Runtime: 27 ms (47.03%) | Memory: 46.7 MB | 2026-09-16

---
+++ leetcode/easy/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character,Integer> map=new LinkedHashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                return s.indexOf(entry.getKey());
+            }
+        }
+        return -1;
+    }
+}
