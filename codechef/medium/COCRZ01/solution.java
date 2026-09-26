cdaccc81c4db38fa3865a5ffb5c30c3957785a67
Boddupalli Venkata Harika <bvharika0906@gmail.com>
2026-09-26 12:29:10 +0530

    ✅ COCRZ01 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-26

--- codechef/medium/COCRZ01/solution.java
+++ codechef/medium/COCRZ01/solution.java
@@ -1,31 +1,17 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		int T=sc.nextInt();
-		while(T-->0){
-		    int n=sc.nextInt();
-		    int k=sc.nextInt();
-		    int arr[]=new int[n];
-		    for(int i=0;i<n;i++){
-		       arr[i]=sc.nextInt();
-		    }
-		    k=k%n;
-		    StringBuilder sb=new StringBuilder();
-		    for(int i=k;i<n;i++){
-		        sb.append(arr[i]).append(" ");
-		    }
-		    for(int i=0;i<k;i++){
-		        sb.append(arr[i]).append(" ");
-		    }
-		    System.out.println(sb.toString().trim());
-		}
-		sc.close();
-	}
+class Solution {
+    public int findMinimumRemovals(List<int[]> intervalList) {
+        // write your code here 
+        intervalList.sort((a,b)->Integer.compare(a[1],b[1]));
+        int sel=0;
+        int preEnd=Integer.MIN_VALUE;
+        for(int[] interval : intervalList){
+            int start=interval[0];
+            int end=interval[1];
+            if(start >= preEnd){
+                sel++;
+                preEnd=end;
+            }
+        }
+        return intervalList.size()-sel;
+    }
 }
